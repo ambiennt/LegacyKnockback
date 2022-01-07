30e7cf23ad5b11b44c8f8beb4c209497d8ab6320
ambiennt <ahook701@gmail.com>
2022-01-07 02:13:34 -0800

    remove whitespace + optimizations

--- src/main.cpp
+++ src/main.cpp
@@ -10,177 +10,173 @@ static std::unordered_map<Player*, uint64_t> attackTimestamp;
 void dllenter() {}
 void dllexit() {}
 void PreInit() {
-    GetServerSymbolWithOffset<PatchSpan<8>>("?doKnockbackAttack@KnockbackRules@@YAXAEAVMob@@0AEBVVec2@@M@Z", 0x117)
-        ->VerifyPatchFunction({0xF3, 0x0F, 0x59, 0x1D, 0x59, 0x6D, 0xAB, 0x00}, NopFilled{}); // F3 0F 59 1D 59 6D AB 00
-    GetServerSymbolWithOffset<PatchSpan<8>>("?doKnockbackAttack@KnockbackRules@@YAXAEAVMob@@0AEBVVec2@@M@Z", 0x124)
-        ->VerifyPatchFunction({0xF3, 0x0F, 0x59, 0x15, 0x4C, 0x6D, 0xAB, 0x00}, NopFilled{}); // F3 0F 59 15 4C 6D AB 00
+	GetServerSymbolWithOffset<PatchSpan<8>>("?doKnockbackAttack@KnockbackRules@@YAXAEAVMob@@0AEBVVec2@@M@Z", 0x117)
+		->VerifyPatchFunction({0xF3, 0x0F, 0x59, 0x1D, 0x59, 0x6D, 0xAB, 0x00}, NopFilled{}); // F3 0F 59 1D 59 6D AB 00
+	GetServerSymbolWithOffset<PatchSpan<8>>("?doKnockbackAttack@KnockbackRules@@YAXAEAVMob@@0AEBVVec2@@M@Z", 0x124)
+		->VerifyPatchFunction({0xF3, 0x0F, 0x59, 0x15, 0x4C, 0x6D, 0xAB, 0x00}, NopFilled{}); // F3 0F 59 15 4C 6D AB 00
 
-    // prevent projectiles from bypassing hurt cooldown by setting newEntityAttack check to false
-    if (!settings.projectilesBypassHurtCooldown) {
-        GetServerSymbolWithOffset<PatchSpan<3>>("?hurtEffects@Mob@@UEAA_NAEBVActorDamageSource@@H_N1@Z", 0x84)
-            ->VerifyPatchFunction({0x83, 0xF8, 0x02}, {0x83, 0xF8, 0x20}); // 83 F8 02 - set actorDamageCause to enum type 32 (invalid cause)
-        GetServerSymbolWithOffset<PatchSpan<3>>("?_hurt@Mob@@MEAA_NAEBVActorDamageSource@@H_N1@Z", 0x48)
-            ->VerifyPatchFunction({0x83, 0xF8, 0x02}, {0x83, 0xF8, 0x20}); // 83 F8 02
-    }
+	// prevent projectiles from bypassing hurt cooldown by setting newEntityAttack check to false
+	if (!settings.projectilesBypassHurtCooldown) {
+		GetServerSymbolWithOffset<PatchSpan<3>>("?hurtEffects@Mob@@UEAA_NAEBVActorDamageSource@@H_N1@Z", 0x84)
+			->VerifyPatchFunction({0x83, 0xF8, 0x02}, {0x83, 0xF8, 0x20}); // 83 F8 02 - set actorDamageCause to enum type 32 (invalid cause)
+		GetServerSymbolWithOffset<PatchSpan<3>>("?_hurt@Mob@@MEAA_NAEBVActorDamageSource@@H_N1@Z", 0x48)
+			->VerifyPatchFunction({0x83, 0xF8, 0x02}, {0x83, 0xF8, 0x20}); // 83 F8 02
+	}
 
-    Mod::PlayerDatabase::GetInstance().AddListener(SIG("left"), [](Mod::PlayerEntry const &entry) {
-        dmgSource.erase(entry.player);
-        hasResetSprint.erase(entry.player);
-        attackTimestamp.erase(entry.player);
-    });
+	Mod::PlayerDatabase::GetInstance().AddListener(SIG("left"), [](Mod::PlayerEntry const &entry) {
+		dmgSource.erase(entry.player);
+		hasResetSprint.erase(entry.player);
+		attackTimestamp.erase(entry.player);
+	});
 }
 void PostInit() {}
 
 inline bool isComboProjectile(ActorType type) { // projectiles used for PVP combos
-    switch (type) {
-        case ActorType::FishingHook:
-        case ActorType::Snowball:
-        case ActorType::ThrownEgg:
-            return true;
-            
-        default: return false;
-    }
+	switch (type) {
+		case ActorType::FishingHook:
+		case ActorType::Snowball:
+		case ActorType::ThrownEgg:
+			return true;
+
+		default: return false;
+	}
 }
 
 TClasslessInstanceHook(bool, "?useLegacyKnockback@KnockbackRules@@YA_NAEBVLevel@@@Z", void* level) { return true; }
 
 THook(float, "?getArmorKnockbackResistance@ArmorItem@@UEBAMXZ", ArmorItem *item) {
-     if (item->mModelIndex == 7) return settings.netheriteArmorKnockbackResistance;
-     return original(item);
+	 if (item->mModelIndex == 7) return settings.netheriteArmorKnockbackResistance;
+	 return original(item);
 }
 
 THook(bool, "?attack@Player@@UEAA_NAEAVActor@@@Z", Player *player, Actor *actor) {
-    attackTimestamp[player] = LocateService<Level>()->GetServerTick();
-    bool result = original(player, actor);
-    if (settings.useJavaSprintReset && (actor->getEntityTypeId() == ActorType::Player_0)) {
-        ((Player*)actor)->setSprinting(false);
-    }
-    return result;
+	attackTimestamp[player] = LocateService<Level>()->GetServerTick();
+	bool result = original(player, actor);
+	if (settings.useJavaSprintReset && (actor->getEntityTypeId() == ActorType::Player_0)) {
+		((Player*)actor)->setSprinting(false);
+	}
+	return result;
 }
 
 THook(void, "?setSprinting@Mob@@UEAAX_N@Z", Mob *mob, bool shouldSprint) {
-    //std::cout << "0x" << std::hex << (int64_t)_ReturnAddress() - (int64_t)GetModuleHandle(0) << std::endl;
-    if (mob->getEntityTypeId() == ActorType::Player_0) {
-        uint64_t returnAddress = ((int64_t)_ReturnAddress() - (int64_t)GetModuleHandle(0));
-        if (returnAddress == 0x3AA84A) { //sprint stopped via user input
-            hasResetSprint.insert(mob);
-        }
-        else if ((!settings.useLegacySprintReset || settings.useJavaSprintReset) || (!settings.useLegacySprintReset && !settings.useJavaSprintReset))  {
-            if (returnAddress == 0x71E634) return; //sprint force-stopped when attacking
-        }
-    }
-    original(mob, shouldSprint);
+	//std::cout << "0x" << std::hex << (int64_t)_ReturnAddress() - (int64_t)GetModuleHandle(0) << std::endl;
+	if (mob->getEntityTypeId() == ActorType::Player_0) {
+		uint64_t returnAddress = ((int64_t)_ReturnAddress() - (int64_t)GetModuleHandle(0));
+		if (returnAddress == 0x3AA84A) { //sprint stopped via user input
+			hasResetSprint.insert(mob);
+		}
+		else if ((!settings.useLegacySprintReset || settings.useJavaSprintReset) || (!settings.useLegacySprintReset && !settings.useJavaSprintReset)) {
+			if (returnAddress == 0x71E634) return; //sprint force-stopped when attacking
+		}
+	}
+	original(mob, shouldSprint);
 }
 
 THook(bool, "?_hurt@Player@@MEAA_NAEBVActorDamageSource@@H_N1@Z", Player *player, ActorDamageSource &source, int dmg, bool knock, bool ignite) {
-    dmgSource[player] = source.getDamagingEntityType();
-    return original(player, source, dmg, knock, ignite);
+	dmgSource[player] = source.getDamagingEntityType();
+	return original(player, source, dmg, knock, ignite);
 }
 
 THook(void, "?knockback@ServerPlayer@@UEAAXPEAVActor@@HMMMMM@Z",
-    ServerPlayer *player, Actor *source, int dmg, float xd, float zd, float power, float height, float heightCap) {
+	ServerPlayer *player, Actor *source, int dmg, float xd, float zd, float power, float height, float heightCap) {
 
-    // bug: vanilla calls punch arrows to do knockback on creative players
-    if ((player->mPlayerGameType == GameType::Creative) || (player->mPlayerGameType == GameType::CreativeViewer)) return;
+	// bug: vanilla calls punch arrows to do knockback on creative players
+	if ((player->mPlayerGameType == GameType::Creative) || (player->mPlayerGameType == GameType::CreativeViewer)) return;
 
-    auto lastHurtCause = player->mLastHurtCause;
-    bool fromEntity = (lastHurtCause == ActorDamageCause::EntityAttack);
-    bool fromProjectile = (lastHurtCause == ActorDamageCause::Projectile);
+	auto lastHurtCause = player->mLastHurtCause;
+	bool fromEntity = (lastHurtCause == ActorDamageCause::EntityAttack);
+	bool fromProjectile = (lastHurtCause == ActorDamageCause::Projectile);
 
-    float punchEnchantmentMultiplier = 1.0f;
-    if (((int64_t)_ReturnAddress() - (int64_t)GetModuleHandle(0)) == 0x273BFE) { //hack: get punch knockback value from arrows
-        punchEnchantmentMultiplier = power; // enchantment tier * 1.6
-    }
-    
-    // initialize values
-    power = settings.normalKnockbackPower;
-    height = settings.normalKnockbackHeight;
-    heightCap = settings.heightCap;
+	float punchEnchantmentMultiplier = 1.0f;
+	if (((int64_t)_ReturnAddress() - (int64_t)GetModuleHandle(0)) == 0x273BFE) { //hack: get punch knockback value from arrows
+		punchEnchantmentMultiplier = power; // enchantment tier * 1.6
+	}
 
-    if (fromProjectile) {
+	// initialize values
+	power = settings.normalKnockbackPower;
+	height = settings.normalKnockbackHeight;
+	heightCap = settings.heightCap;
 
-        if (settings.customProjectileKnockbackEnabled) {
+	if (fromProjectile) {
 
-            if (isComboProjectile(dmgSource[player])) {
-                power = settings.comboProjectileKnockbackPower;
-                height = settings.comboProjectileKnockbackHeight;
-            }
+		if (settings.customProjectileKnockbackEnabled) {
 
-            else if (dmgSource[player] == ActorType::Enderpearl) {
-                power = settings.enderpearlKnockbackPower;
-                height = settings.enderpearlKnockbackHeight;
-            }
-        }
-        power *= punchEnchantmentMultiplier;
-    }
-        
-    else if (source && fromEntity) {
+			if (isComboProjectile(dmgSource[player])) {
+				power = settings.comboProjectileKnockbackPower;
+				height = settings.comboProjectileKnockbackHeight;
+			}
 
-        auto attacker = ((Mob*)source);
-        float kbEnchantmentBonus = ((float)(attacker->getMeleeKnockbackBonus()) * 0.4f);
+			else if (dmgSource[player] == ActorType::Enderpearl) {
+				power = settings.enderpearlKnockbackPower;
+				height = settings.enderpearlKnockbackHeight;
+			}
+		}
+		power *= punchEnchantmentMultiplier;
+	}
 
-        if (hasResetSprint.count(attacker) && attacker->isSprinting()) {
-            hasResetSprint.erase(attacker);
-            power += settings.additionalWTapKnockbackPower;
-            height += settings.additionalWTapKnockbackHeight;
-        }
+	else if (source && fromEntity) {
 
-        // attacking on the same tick that you receive knockback will set lower values (aka "reducing")
-        uint64_t currentTick = LocateService<Level>()->GetServerTick();
-        if ((currentTick - attackTimestamp[player]) <= 1) {
-            power *= settings.knockbackReductionFactor;
-        }
-        power += kbEnchantmentBonus;
-    }
+		auto attacker = ((Mob*)source);
+		float kbEnchantmentBonus = ((float)(attacker->getMeleeKnockbackBonus()) * 0.4f);
 
-    // calculate knockback resistance
-    float knockbackResistanceValue = getAttribute(player, 9)->currentVal;
-    float scaledKnockbackForce = std::fmax(1.0f - knockbackResistanceValue, 0.0f);
+		if (hasResetSprint.count(attacker) && attacker->isSprinting()) {
+			hasResetSprint.erase(attacker);
+			power += settings.additionalWTapKnockbackPower;
+			height += settings.additionalWTapKnockbackHeight;
+		}
 
-    if (scaledKnockbackForce < 1.0f) {
-        power *= scaledKnockbackForce;
-        height *= scaledKnockbackForce;
-    }
+		// attacking on the same tick that you receive knockback will set lower values (aka "reducing")
+		uint64_t currentTick = LocateService<Level>()->GetServerTick();
+		if ((currentTick - attackTimestamp[player]) <= 1) {
+			power *= settings.knockbackReductionFactor;
+		}
+		power += kbEnchantmentBonus;
+	}
 
-    // auto posDelta = player->mStateVectorComponent.mPosDelta;
-    auto posDelta = player->mTeleportedThisTick ? Vec3::ZERO : player->getPosDelta();
-    float oldPosDeltaY = posDelta.y;
-    float f = sqrt(xd * xd + zd * zd);
-    if (f <= 0.0f) return;
+	// calculate knockback resistance
+	float knockbackResistanceValue = getAttribute(player, 9)->currentVal;
+	float scaledKnockbackForce = std::fmax(1.0f - knockbackResistanceValue, 0.0f);
 
-    if (settings.useJavaHeightCap) {
-        posDelta.x /= settings.knockbackFriction;
-        posDelta.y /= settings.knockbackFriction;
-        posDelta.z /= settings.knockbackFriction;
+	if (scaledKnockbackForce < 1.0f) {
+		power *= scaledKnockbackForce;
+		height *= scaledKnockbackForce;
+	}
 
-        posDelta.x -= xd / f * power;
-        posDelta.y += height;
-        posDelta.z -= zd / f * power;
+	// auto posDelta = player->mStateVectorComponent.mPosDelta;
+	auto posDelta = player->mTeleportedThisTick ? Vec3::ZERO : player->getPosDelta();
+	float oldPosDeltaY = posDelta.y;
+	float f = sqrt(xd * xd + zd * zd);
+	if (f <= 0.0f) return;
+	float f1 = 1.0f / f;
+	float f2 = 1.0f / settings.knockbackFriction;
 
-        if (posDelta.y > settings.heightThreshold) {
-            posDelta.y = heightCap;
-        }
-    }
-    else {
-        posDelta.x /= settings.knockbackFriction;
-        posDelta.z /= settings.knockbackFriction;
+	posDelta.x *= f2;
+	posDelta.z *= f2;
 
-        posDelta.x -= xd / f * power;
-        posDelta.y = height;
-        posDelta.z -= zd / f * power;
+	posDelta.x -= xd * f1 * power;
+	posDelta.z -= zd * f1 * power;
 
-        if (settings.useCustomHeightCap && (oldPosDeltaY + posDelta.y > settings.heightThreshold)) {
-            posDelta.y = heightCap;
-        }
-    }
+	if (settings.useJavaHeightCap) {
+		posDelta.y *= f2;
+		posDelta.y += height;
 
-    /*float healthValue = getAttribute(player, 7)->currentVal;
-    if (healthValue <= 0.0f) {
-        player->mIsKnockedBackOnDeath = true; // client ignores motion packets if it's dead anyway
-    }*/
+		if (posDelta.y > settings.heightThreshold) {
+			posDelta.y = heightCap;
+		}
+	}
+	else {
+		posDelta.y = height;
 
-    SetActorMotionPacket pkt;
-    pkt.mRuntimeId = player->mRuntimeID;
-    pkt.mMotion = posDelta;
-    player->mDimension->sendPacketForEntity(*player, pkt, nullptr);
+		if (settings.useCustomHeightCap && (oldPosDeltaY + posDelta.y > settings.heightThreshold)) {
+			posDelta.y = heightCap;
+		}
+	}
+
+	/*float healthValue = getAttribute(player, 7)->currentVal;
+	if (healthValue <= 0.0f) {
+		player->mIsKnockedBackOnDeath = true; // client ignores motion packets if it's dead anyway
+	}*/
+
+	SetActorMotionPacket pkt(player->mRuntimeID, posDelta);
+	player->mDimension->sendPacketForEntity(*player, pkt, nullptr);
 }
--- src/main.h
+++ src/main.h
@@ -20,47 +20,47 @@
 #include <Item/ArmorItem.h>
 
 inline struct Settings {
-    float normalKnockbackPower               = 0.4f;
-    float normalKnockbackHeight              = 0.4f;
-    float additionalWTapKnockbackPower       = 0.4f;
-    float additionalWTapKnockbackHeight      = 0.0f;
-    float knockbackReductionFactor           = 0.6f;
-    float knockbackFriction                  = 2.0f;
-    bool customProjectileKnockbackEnabled    = false;
-    float comboProjectileKnockbackPower      = 0.4f;
-    float comboProjectileKnockbackHeight     = 0.4f;
-    float enderpearlKnockbackPower           = 0.4f;
-    float enderpearlKnockbackHeight          = 0.4f;
-    bool useJavaSprintReset                  = true;
-    bool useLegacySprintReset                = false;
-    bool useJavaHeightCap                    = false;
-    bool useCustomHeightCap                  = true;
-    float heightThreshold                    = 0.4f;
-    float heightCap                          = 0.4f;
-    bool projectilesBypassHurtCooldown       = false;
-    float netheriteArmorKnockbackResistance  = 0.0f;
+	float normalKnockbackPower               = 0.4f;
+	float normalKnockbackHeight              = 0.4f;
+	float additionalWTapKnockbackPower       = 0.4f;
+	float additionalWTapKnockbackHeight      = 0.0f;
+	float knockbackReductionFactor           = 0.6f;
+	float knockbackFriction                  = 2.0f;
+	bool customProjectileKnockbackEnabled    = false;
+	float comboProjectileKnockbackPower      = 0.4f;
+	float comboProjectileKnockbackHeight     = 0.4f;
+	float enderpearlKnockbackPower           = 0.4f;
+	float enderpearlKnockbackHeight          = 0.4f;
+	bool useJavaSprintReset                  = true;
+	bool useLegacySprintReset                = false;
+	bool useJavaHeightCap                    = false;
+	bool useCustomHeightCap                  = true;
+	float heightThreshold                    = 0.4f;
+	float heightCap                          = 0.4f;
+	bool projectilesBypassHurtCooldown       = false;
+	float netheriteArmorKnockbackResistance  = 0.0f;
 
-  template <typename IO> static inline bool io(IO f, Settings &settings, YAML::Node &node) { 
-    return f(settings.normalKnockbackPower, node["normalKnockbackPower"]) &&
-           f(settings.normalKnockbackHeight, node["normalKnockbackHeight"]) &&
-           f(settings.additionalWTapKnockbackPower, node["additionalWTapKnockbackPower"]) &&
-           f(settings.additionalWTapKnockbackHeight, node["additionalWTapKnockbackHeight"]) &&
-           f(settings.knockbackReductionFactor, node["knockbackReductionFactor"]) &&
-           f(settings.knockbackFriction, node["knockbackFriction"]) &&
-           f(settings.customProjectileKnockbackEnabled, node["customProjectileKnockbackEnabled"]) &&
-           f(settings.comboProjectileKnockbackPower, node["comboProjectileKnockbackPower"]) &&
-           f(settings.comboProjectileKnockbackHeight, node["comboProjectileKnockbackHeight"]) &&
-           f(settings.enderpearlKnockbackPower, node["enderpearlKnockbackPower"]) &&
-           f(settings.enderpearlKnockbackHeight, node["enderpearlKnockbackHeight"]) &&
-           f(settings.useJavaSprintReset, node["useJavaSprintReset"]) &&
-           f(settings.useLegacySprintReset, node["useLegacySprintReset"]) &&
-           f(settings.useJavaHeightCap, node["useJavaHeightCap"]) &&
-           f(settings.useCustomHeightCap, node["useCustomHeightCap"]) &&
-           f(settings.heightThreshold, node["heightThreshold"]) &&
-           f(settings.heightCap, node["heightCap"]) &&
-           f(settings.projectilesBypassHurtCooldown, node["projectilesBypassHurtCooldown"]) &&
-           f(settings.netheriteArmorKnockbackResistance, node["netheriteArmorKnockbackResistance"]);
-    }
+	template <typename IO> static inline bool io(IO f, Settings &settings, YAML::Node &node) {
+		return f(settings.normalKnockbackPower, node["normalKnockbackPower"]) &&
+			   f(settings.normalKnockbackHeight, node["normalKnockbackHeight"]) &&
+			   f(settings.additionalWTapKnockbackPower, node["additionalWTapKnockbackPower"]) &&
+			   f(settings.additionalWTapKnockbackHeight, node["additionalWTapKnockbackHeight"]) &&
+			   f(settings.knockbackReductionFactor, node["knockbackReductionFactor"]) &&
+			   f(settings.knockbackFriction, node["knockbackFriction"]) &&
+			   f(settings.customProjectileKnockbackEnabled, node["customProjectileKnockbackEnabled"]) &&
+			   f(settings.comboProjectileKnockbackPower, node["comboProjectileKnockbackPower"]) &&
+			   f(settings.comboProjectileKnockbackHeight, node["comboProjectileKnockbackHeight"]) &&
+			   f(settings.enderpearlKnockbackPower, node["enderpearlKnockbackPower"]) &&
+			   f(settings.enderpearlKnockbackHeight, node["enderpearlKnockbackHeight"]) &&
+			   f(settings.useJavaSprintReset, node["useJavaSprintReset"]) &&
+			   f(settings.useLegacySprintReset, node["useLegacySprintReset"]) &&
+			   f(settings.useJavaHeightCap, node["useJavaHeightCap"]) &&
+			   f(settings.useCustomHeightCap, node["useCustomHeightCap"]) &&
+			   f(settings.heightThreshold, node["heightThreshold"]) &&
+			   f(settings.heightCap, node["heightCap"]) &&
+			   f(settings.projectilesBypassHurtCooldown, node["projectilesBypassHurtCooldown"]) &&
+			   f(settings.netheriteArmorKnockbackResistance, node["netheriteArmorKnockbackResistance"]);
+		}
 } settings;
 
 DEF_LOGGER("LegacyKnockback");
